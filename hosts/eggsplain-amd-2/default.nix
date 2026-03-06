f33a77e16edfbaaa0f05ff3b0054d27014d48412
amubashir200018+github@gmail.com <amubashir200018+github@gmail.com>
2026-03-06 22:13:28 +0500

    feat: added git

--- hosts/eggsplain-amd-2/default.nix
+++ hosts/eggsplain-amd-2/default.nix
@@ -5,30 +5,69 @@
         ./../_setups/mandatory/base
         ./../_setups/mandatory/bootloader/uefi.nix
         ./../_setups/mandatory/desktop/gnome-xorg.nix
-        ./../_setups/mandatory/locale/english.nix
+        ./../_setups/mandatory/locale/stockholm.nix
 
         ./../_setups/optional/boot-logo
-        # ./../_setups/optional/home-manager
-        # ./../_setups/optional/home-manager/build-iso/cd-dvd.nix
-        ./../_setups/optional/auto-login.nix
         ./../_setups/optional/amdgpu+blacklist-radeon.nix
         ./../_setups/optional/docker.nix
         ./../_setups/optional/firefox-simple.nix
         ./../_setups/optional/rustdesk.nix
+        ./../_setups/optional/ssh-tunnels.nix
+        ./../_setups/optional/dokploy.nix
+        ./../_setups/optional/sudo-nopasswd.nix
+        ./../_setups/optional/latest-kernel.nix
     ];
 
     networking.hostName = "eggsplain-amd-2";
 
-    services.teamviewer.enable = true;
+    # SSH Tunnels
+    services.ssh-tunnels = {
+        enable = true;
+        tunnels = {
+            prod = {
+                description = "Reverse SSH Tunnel to Prod VPS";
+                remoteForward = "0.0.0.0:8890:localhost:22";
+                vpsHost = "72.60.35.185";
+            };
+            staging = {
+                description = "Reverse SSH Tunnel to Staging VPS";
+                remoteForward = "0.0.0.0:8890:localhost:22";
+                vpsHost = "31.97.78.33";
+            };
+        };
+    };
 
-    # This defaults to podman
+    # Dokploy
+    services.dokploy.enable = true;
+
+    # Sudo
+    security.sudo-nopasswd.enable = true;
+
+    # System Packages
+    environment.systemPackages = with pkgs; [
+        vim
+        wget
+        git
+        nodejs
+        vscode
+        firefox
+        rustdesk
+    ];
+
+    # User Account (partially handled by base but extending with packages)
+    users.users.${username} = {
+        packages = with pkgs; [
+            rustdesk
+            vscode
+            nodejs
+            git
+            firefox
+        ];
+    };
+
+    # OCI Backend
     virtualisation.oci-containers.backend = "docker";
 
-    virtualisation.oci-containers.containers = {};
-
-    environment.systemPackages = with pkgs; [
-        teamviewer
-        anydesk
-        vscode
-    ];
+    # State Version
+    system.stateVersion = "25.11";
 }
