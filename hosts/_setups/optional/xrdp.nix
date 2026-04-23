2216f564b59f3373d79915ad2a67ae34f4db7f4b
amubashir200018+github@gmail.com <amubashir200018+github@gmail.com>
2026-04-23 12:22:10 +0500

    feat: added gnome session for amd-2

--- hosts/_setups/optional/xrdp.nix
+++ hosts/_setups/optional/xrdp.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
     # Enable XRDP Server for Guacamole Connection
     services.xrdp.enable = true;
-    services.xrdp.defaultWindowManager = "gnome-session";
+    services.xrdp.defaultWindowManager = ''
+        if [ -z "$LANG" ]; then
+            export LANG="en_US.UTF-8"
+        fi
+        
+        exec dbus-run-session gnome-session
+    '';
     services.xrdp.openFirewall = true;
 }
